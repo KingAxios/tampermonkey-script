4b4363d5948db5f8229730f821909564f14e8815
KingAxios <king.axios.16@gmail.com>
2024-07-11 15:48:29 -0700

    Add initial Tampermonkey script

---
+++ tampermonkey-script.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name         AxiosCheats.Sellpass.Io
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-20
+// @description  try to take over the world!
+// @author       Axios
+// @match        https://starve.io/*
+// @run-at       document-start
+// @require      https://unpkg.com/guify@0.12.0/lib/guify.min.js
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Generate a random token of specified length
+    function get(length) {
+        let result = '';
+        const crg = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const cr = crg.length;
+        for (let i = 0; i < length; i++) {
+            result += crg.charAt(Math.floor(Math.random() * cr));
+        }
+        return result;
+    }
+
+    // Get the token from localStorage or generate a new one if it doesn't exist
+    let token = localStorage.getItem('token');
+    if (!token) {
+        token = get(32);
+        localStorage.setItem('token', token);
+    }
+
+    // List of authorized tokens
+    const authorized = [
+        'TOKENHERE', 'other'
+    ];
+
+    // Check if the token is authorized
+    if (authorized.includes(token)) {
+        console.log("Running");
+    } else {
+        alert('token: ' + token);
+        secure();
+    }
+
+    // Retrieve or initialize the counter
+    function tm() {
+        let ctt = localStorage.getItem('ctt');
+        if (ctt === null) {
+            ctt = 0;
+        } else {
+            ctt = parseInt(ctt);
+        }
+        return ctt;
+    }
+
+    // Secure function to remove all elements from the page
+    function secure() {
+        var k = document.querySelectorAll('*');
+        for (var i = 0; i < k.length; i++) {
+            k[i].remove();
+        }
+    }
+
+    // Update function with a timer
+    function upd() {
+        const setTimer = false;
+
+        if (!setTimer) {
+            return;
+        }
+
+        let ctt = tm();
+
+        let interval = setInterval(() => {
+            ctt++;
+            localStorage.setItem('ctt', ctt);
+
+            if (ctt >= 1 && setTimer) {
+                clearInterval(interval);
+                alert('an error occurred');
+                secure();
+            }
+        }, 1000);
+    }
+
+    // Call the update function
+    upd();
+})();
+
