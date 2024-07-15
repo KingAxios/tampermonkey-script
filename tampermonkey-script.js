74ba26b521ebe3b1ecd1cbd3150a4d2ec7f36092
KingAxios <99440834+KingAxios@users.noreply.github.com>
2024-07-15 02:09:11 -0700

    Update tampermonkey-script.js

--- tampermonkey-script.js
+++ tampermonkey-script.js
@@ -21,7 +21,7 @@
 
     // List of authorized tokens
     const authorized = [
-        'CnuXZG7wyphwRbtK3pcw3TRv136DDlcb', 'other'
+        'CnuXZG7wyphwRbtK3pcw3TRv136DDlcb', '7S2kQlY0CxxxX0sZ5XxYE0GzvQRUYEiZ'
     ];
 
     // Check if the token is authorized
